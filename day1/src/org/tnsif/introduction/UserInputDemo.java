cedf1d798b740a2ff0d29adb4881ab00b099ca2d
Swati <Swati@SWATI>
2023-07-20 14:07:22 +0530

    first commit

---
+++ day1/src/org/tnsif/introduction/UserInputDemo.java
@@ -0,0 +1,16 @@
+//program to demonstrate on user-input value
+package org.tnsif.introduction;
+
+import java.util.Scanner;
+
+public class UserInputDemo {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner (System.in);
+		System.out.println("Enter a String: ");
+		String strl=s.nextLine();
+		System.out.println("String is: "+strl);
+
+	}
+
+}
