a37f66d7172c01425e18af442f6d1dd48e9faf90
Swati <Swati@SWATI>
2023-09-22 11:55:22 +0530

    commit

---
+++ day13/src/org/tnsif/day13/children.java
@@ -0,0 +1,24 @@
+package org.tnsif.day13;
+
+public class children
+{
+	int rollNo;
+	String name;
+	public int getRollNo() {
+		return rollNo;
+	}
+	public void setRollNo(int rollNo) {
+		this.rollNo = rollNo;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String toString()
+	{
+		return rollNo+"\t"+name;
+	}
+}
